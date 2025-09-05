abd10e7d0e3f946760e79891427c9d4ad551de8c
hutchinsonsam16 <hutchinsonsam.16@gmail.com>
2025-09-05 19:24:26 +0530

    refactor: Simplify `cn` utility function
    Removes redundant imports and direct use of `clsx` and `tailwind-merge` by leveraging the aliased imports. This streamlines the utility function and reduces potential confusion.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,13 +1,6 @@
-import { type ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Since this environment can't easily add dependencies, we use a more direct `cn`
-// implementation that relies on the globally available `clsx` and `tailwind-merge`
-// if they were loaded, or falls back to a simpler join.
-// With Vite, these will be bundled correctly.
-import realClsx from 'clsx';
-import realTwMerge from 'tailwind-merge';
-
 export function cn(...inputs: ClassValue[]) {
-  return realTwMerge(realClsx(inputs));
+  return twMerge(clsx(inputs));
 }
